fbc9ada1d4f3a79ae9d98c044b8f6cfd54390a9e
Barlog <barlog@tanelorn.dev>
2015-09-23 14:14:04 +0300

    Cosmetic changes

--- comparator/src/main/java/com/example/Generator.java
+++ comparator/src/main/java/com/example/Generator.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class Generator {
 	private static final ULocale locale = new ULocale("ru_RU");
-	private static final RuleBasedNumberFormat rule = new com.ibm.icu.text.RuleBasedNumberFormat(locale, RuleBasedNumberFormat.SPELLOUT);
+	private static final RuleBasedNumberFormat rule = new RuleBasedNumberFormat(locale, RuleBasedNumberFormat.SPELLOUT);
 	private static final Random rnd = new Random();
 	private static final int MAX = 9999;
 
